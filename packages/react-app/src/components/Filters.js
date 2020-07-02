72284a498e67f1e41011f21cbf0b9c948e0e6c5d
sripwoud <me@sripwoud.xyz>
2020-07-03 00:22:30 +0200

    feat: Display total of channels filtered

--- packages/react-app/src/components/Filters.js
+++ packages/react-app/src/components/Filters.js
@@ -6,6 +6,7 @@ import { queries } from '../queries'
 
 export const Filters = () => {
   const query = useStoreState(state => state.query.value)
+  const channels = useStoreState(state => state.channels.data)
   const { loading, error, data } = useQuery(query)
 
   const updateChannels = useStoreActions(actions => actions.channels.update)
@@ -17,18 +18,21 @@ export const Filters = () => {
   }, [loading, error, data])
 
   return (
-    <Flex justifyContent='space-around' mb={4}>
-      {Object.keys(queries).map((queryName, index) => {
-        const { queryString, icon } = queries[queryName]
-        return (
-          <Button
-            key={index}
-            icon={icon}
-            onClick={() => updateQuery(queryString)}>
-            {queryName}
-          </Button>
-        )
-      })}
-    </Flex>
+    <>
+      <Flex justifyContent='space-around' mb={4}>
+        {Object.keys(queries).map((queryName, index) => {
+          const { queryString, icon } = queries[queryName]
+          return (
+            <Button
+              key={index}
+              icon={icon}
+              onClick={() => updateQuery(queryString)}>
+              {queryName}
+            </Button>
+          )
+        })}
+      </Flex>
+      <p>{channels.length}</p>
+    </>
   )
 }
