6820339d15895ad558664bb851e710fd37b65ad6
r1oga <r1oga@tuta.io>
2020-07-02 14:51:57 +0200

    feat: Display Header and Footer components

---
+++ packages/react-app/src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Header = props => {
+  return (
+    <>
+      <h1>CHANNELS RAIDAR</h1>
+      <p>Monitoring of Raiden Network Payment Channels</p>
+    </>
+  )
+}
