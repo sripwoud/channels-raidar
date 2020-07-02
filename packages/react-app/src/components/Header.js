ff473dea68e8a0fa8c276193bbc5ff9e49a60503
3pwd <3pwd@tuta.io>
2020-07-02 17:09:28 +0200

    Put Footer content in Header, delete Footer

--- packages/react-app/src/components/Header.js
+++ packages/react-app/src/components/Header.js
@@ -1,9 +1,26 @@
 import React from 'react'
-import { Link, Text } from 'rimble-ui'
+import { Link, Text, Box } from 'rimble-ui'
 
 export const Header = props => {
   return (
     <>
+      <Box
+        alignItems='center'
+        justifyContent='center'
+        BoxDirection='column'
+        mt={3}>
+        Made by
+        <Link href='https://twitter.com/r1oga' target='__blank' px={2}>
+          @r1oga
+        </Link>
+        -
+        <Link
+          href='hhttps://github.com/r1oga/channels-raidar'
+          target='__blank'
+          px={2}>
+          GitHub
+        </Link>
+      </Box>
       <h1>CHANNELS RAIDAR</h1>
       <Text mb={4}>
         Monitoring of
--- packages/react-app/src/components/Layout.js
+++ packages/react-app/src/components/Layout.js
@@ -1,14 +1,12 @@
 import React from 'react'
 
 import { Header } from './Header'
-import { Footer } from './Footer'
 
 export const Layout = props => {
   return (
     <>
       <Header />
       {props.children}
-      <Footer />
     </>
   )
 }
