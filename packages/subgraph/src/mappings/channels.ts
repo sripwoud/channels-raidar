a20e58991f54a92e55f6b2ed8540b43a9262818f
r1oga <r1oga@tuta.io>
2020-07-02 15:15:02 +0200

    feat: Add transaction hash field to subgraph

--- packages/subgraph/src/mappings/channels.ts
+++ packages/subgraph/src/mappings/channels.ts
@@ -8,6 +8,7 @@ import {
 export function handleChannelOpened(event: ChannelOpened): void {
   let id = event.params.channel_identifier.toHex()
   let channel = new Channel(id)
+  channel.tx = event.transaction.hash
   channel.openedAtBlock = event.block.number
   channel.status = 'open'
   channel.participant1 = event.params.participant1
@@ -18,6 +19,7 @@ export function handleChannelOpened(event: ChannelOpened): void {
 export function handleChannelClosed(event: ChannelClosed): void {
   let id = event.params.channel_identifier.toHex()
   let channel = Channel.load(id)
+  channel.tx = event.transaction.hash
   channel.closedBy = event.params.closing_participant
   channel.closingNonce = event.params.nonce
   channel.status = 'closed'
@@ -27,6 +29,7 @@ export function handleChannelClosed(event: ChannelClosed): void {
 export function handleChannelSettled(event: ChannelSettled): void {
   let id = event.params.channel_identifier.toHex()
   let channel = Channel.load(id)
+  channel.tx = event.transaction.hash
   channel.settled_amount1 = event.params.participant1_amount
   channel.settled_amount2 = event.params.participant2_amount
   channel.status = 'settled'
