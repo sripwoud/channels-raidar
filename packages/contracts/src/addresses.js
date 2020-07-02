af8ee83f06b46bc7285457f27885e89e4a57d701
sripwoud <me@sripwoud.xyz>
2020-07-02 11:42:16 +0200

    Update ABI files and contract addresses

--- packages/contracts/src/addresses.js
+++ packages/contracts/src/addresses.js
@@ -1,7 +1,8 @@
 // This address points to a dummy ERC20 contract deployed on Ethereum Mainnet,
 // Goerli, Kovan, Rinkeby and Ropsten. Replace it with your smart contracts.
 const addresses = {
-  ceaErc20: "0xc1C0472c0C80bCcDC7F5D01A376Bd97a734B8815",
-};
+  tttNetwork: '0x3EA2a1fED7FdEf300DA19E97092Ce8FdF8bf66A3',
+  tttErc20: '0x59105441977ecD9d805A4f5b060E34676F50F806'
+}
 
-export default addresses;
+export default addresses
