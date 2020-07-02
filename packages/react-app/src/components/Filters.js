b964cc58adfd618073dad162c0b78e5d1f958069
3pwd <3pwd@tuta.io>
2020-07-02 16:49:43 +0200

    feat: Filter channels per status

--- packages/react-app/src/components/Filters.js
+++ packages/react-app/src/components/Filters.js
@@ -1,17 +1,28 @@
 import React, { useEffect } from 'react'
-import { Button } from 'rimble-ui'
-import { useStoreActions } from 'easy-peasy'
+import { Button, Flex } from 'rimble-ui'
+import { useStoreActions, useStoreState } from 'easy-peasy'
 import { useQuery } from '@apollo/react-hooks'
 import { queries } from '../queries'
 
 export const Filters = () => {
-  const { loading, error, data } = useQuery(queries.all)
+  const query = useStoreState(state => state.query.value)
+  const { loading, error, data } = useQuery(query)
 
   const updateChannels = useStoreActions(actions => actions.channels.update)
+  const updateQuery = useStoreActions(actions => actions.query.update)
   useEffect(() => {
     if (!loading && !error && data?.channels) {
       updateChannels(data.channels)
     }
   }, [loading, error, data])
-  return <Button>All</Button>
+
+  return (
+    <Flex justifyContent='space-around' mb={4}>
+      {Object.keys(queries).map((queryName, index) => (
+        <Button key={index} onClick={() => updateQuery(queries[queryName])}>
+          {queryName}
+        </Button>
+      ))}
+    </Flex>
+  )
 }
