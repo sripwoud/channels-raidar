151ce3f580ae727362301a4d10f0f89ecc062f64
3pwd <3pwd@tuta.io>
2020-07-03 13:41:15 +0200

    refactor: Use query a builder function

--- packages/react-app/src/queries.ts
+++ packages/react-app/src/queries.ts
@@ -1,145 +1,137 @@
 import { gql } from 'apollo-boost'
-import { ChannelStatus, AddressesFilters } from './constants'
+import { ChannelFilter } from './constants'
 
-export const queries = {
-  all: {
-    all: {
-      queryString: gql`
-        {
-          channels(orderBy: openedAtBlock, orderDirection: desc) {
-            tx
-            status
-            participants
-            closedBy
-            settled_amounts
-          }
-        }
-      `,
-      icon: 'SelectAll'
-    },
-    [ChannelStatus.open]: {
-      queryString: gql`
-        {
-          channels(
-            where: { status: open }
-            orderBy: openedAtBlock
-            orderDirection: desc
-          ) {
-            status
-            tx
-            participants
-          }
-        }
-      `,
-      icon: 'LockOpen'
-    },
-    [ChannelStatus.closed]: {
-      queryString: gql`
-        {
-          channels(
-            where: { status: closed }
-            orderBy: openedAtBlock
-            orderDirection: desc
-          ) {
-            status
-            tx
-            participants
-            closedBy
-          }
-        }
-      `,
-      icon: 'LockOutline'
-    },
-    [ChannelStatus.settled]: {
-      queryString: gql`
-        {
-          channels(
-            where: { status: settled }
-            orderBy: openedAtBlock
-            orderDirection: desc
-          ) {
-            status
-            tx
-            participants
-            closedBy
-            settled_amounts
-          }
-        }
-      `,
-      icon: 'Gavel'
+const queriesAll = {
+  all: gql`
+    {
+      channels(orderBy: openedAtBlock, orderDirection: desc) {
+        tx
+        status
+        participants
+        closedBy
+        settled_amounts
+      }
     }
-  },
-  one: {
-    all: {
-      queryString: gql`
-        {
-          channels(orderBy: openedAtBlock, orderDirection: desc) {
-            tx
-            status
-            participant1
-            participant2
-            closedBy
-            settled_amount1
-            settled_amount2
-          }
-        }
-      `,
-      icon: 'SelectAll'
-    },
-    [ChannelStatus.open]: {
-      queryString: gql`
-        {
-          channels(
-            where: { status: open }
-            orderBy: openedAtBlock
-            orderDirection: desc
-          ) {
-            status
-            tx
-            participant1
-            participant2
-          }
-        }
-      `,
-      icon: 'LockOpen'
-    },
-    [ChannelStatus.closed]: {
-      queryString: gql`
-        {
-          channels(
-            where: { status: closed }
-            orderBy: openedAtBlock
-            orderDirection: desc
-          ) {
-            status
-            tx
-            participant1
-            participant2
-            closedBy
-          }
-        }
-      `,
-      icon: 'LockOutline'
-    },
-    [ChannelStatus.settled]: {
-      queryString: gql`
-        {
-          channels(
-            where: { status: settled }
-            orderBy: openedAtBlock
-            orderDirection: desc
-          ) {
-            status
-            tx
-            participant1
-            participant2
-            closedBy
-            settled_amount1
-            settled_amount2
-          }
-        }
-      `,
-      icon: 'Gavel'
+  `,
+  [ChannelFilter.open]: gql`
+    {
+      channels(
+        where: { status: open }
+        orderBy: openedAtBlock
+        orderDirection: desc
+      ) {
+        status
+        tx
+        participants
+      }
     }
+  `,
+  [ChannelFilter.closed]: gql`
+    {
+      channels(
+        where: { status: closed }
+        orderBy: openedAtBlock
+        orderDirection: desc
+      ) {
+        status
+        tx
+        participants
+        closedBy
+      }
+    }
+  `,
+  [ChannelFilter.settled]: gql`
+    {
+      channels(
+        where: { status: settled }
+        orderBy: openedAtBlock
+        orderDirection: desc
+      ) {
+        status
+        tx
+        participants
+        closedBy
+        settled_amounts
+      }
+    }
+  `
+}
+
+const queriesOne = (addressFilter: string) => {
+  return {
+    all: gql`
+      {
+        channels(
+          where: { participants_contains: [${addressFilter}] }
+          orderBy: openedAtBlock
+          orderDirection: desc
+        ) {
+          status
+          tx
+          participants
+          closedBy
+          settle_amounts
+        }
+      }
+    `,
+    [ChannelFilter.open]: gql`
+      {
+        channels(
+          where: {
+            status: open,
+            participants_contains: [${addressFilter}]
+          }
+          orderBy: openedAtBlock
+          orderDirection: desc
+        ) {
+          status
+          tx
+          participants
+        }
+      }
+    `,
+    [ChannelFilter.closed]: gql`
+      {
+        channels(
+          where: {
+            status: closed
+            participants_contains: [${addressFilter}]
+          }
+          orderBy: openedAtBlock
+          orderDirection: desc
+        ) {
+          status
+          tx
+          participants
+          closedBy
+        }
+      }
+    `,
+    [ChannelFilter.settled]: gql`
+      {
+        channels(
+          where: {
+            status: settled
+            participants_contains: [${addressFilter}]
+          }
+          orderBy: openedAtBlock
+          orderDirection: desc
+        ) {
+          status
+          tx
+          participants
+          closedBy
+          settled_amounts
+        }
+      }
+    `
   }
+}
+
+export const queries = (
+  addressFilter: string,
+  channelFilter: ChannelFilter
+) => {
+  if (addressFilter === 'all') return queriesAll[channelFilter]
+  return queriesOne(addressFilter)[channelFilter]
 }
