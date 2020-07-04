fdc65d27ddff46f35357a9caad8c4d3db1059fcf
sripwoud <me@sripwoud.xyz>
2020-07-04 19:31:09 +0200

    refactor: Extract enable metamask logic in App.js

--- packages/react-app/src/App.js
+++ packages/react-app/src/App.js
@@ -1,33 +1,62 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Flex } from 'rimble-ui'
 import { useStoreState } from 'easy-peasy'
+import { useWeb3React } from '@web3-react/core'
+import { InjectedConnector } from '@web3-react/injected-connector'
 import { Layout } from './components/Layout'
 import { Channel } from './components/Channel'
 import { Form } from './components/Form'
-// import logo from './ethereumLogo.png'
-// import { addresses, abis } from '@project/contracts'
-// import { ethers } from 'ethers'
 import './App.css'
 
-// async function readOnchainBalance() {
-//   // Should replace with the end-user wallet, e.g. Metamask
-//   const defaultProvider = ethers.getDefaultProvider()
-//   // Create an instance of ethers.Contract
-//   // Read more about ethers.js on https://docs.ethers.io/ethers.js/html/api-contract.html
-//   const ceaErc20 = new ethers.Contract(
-//     addresses.ceaErc20,
-//     abis.erc20,
-//     defaultProvider
-//   )
-//   // A pre-defined address that owns some CEAERC20 tokens
-//   const tokenBalance = await ceaErc20.balanceOf(
-//     '0x3f8CB69d9c0ED01923F11c829BaE4D9a4CB6c82C'
-//   )
-//   console.log({ tokenBalance: tokenBalance.toString() })
-// }
+const { ethereum } = window
+const injected = new InjectedConnector({
+  /* Need to include all chains that can be selected in metamask
+    in supportedChainIds so that the connection banner
+    can display warning messages accordingly
+  */
+  supportedChainIds: [1, 2, 3, 4, 42, 5]
+})
 
 export default () => {
   const channels = useStoreState(state => state.channels.data)
+  const context = useWeb3React()
+  const { activate, active, error } = context
+
+  if (ethereum && ethereum.on && !active && !error) {
+    activate(injected)
+  }
+
+  useEffect(() => {
+    if (ethereum && ethereum.on && !active && !error) {
+      const handleChainChanged = chainId => {
+        activate(injected)
+      }
+
+      const handleAccountsChanged = accounts => {
+        if (accounts.length > 0) {
+          activate(injected)
+        }
+      }
+
+      const handleNetworkChanged = networkId => {
+        activate(injected)
+      }
+
+      ethereum.on('chainChanged', handleChainChanged)
+      ethereum.on('accountsChanged', handleAccountsChanged)
+      ethereum.on('networkChanged', handleNetworkChanged)
+
+      return () => {
+        if (ethereum.removeListener) {
+          ethereum.removeListener('chainChanged', handleChainChanged)
+          ethereum.removeListener('accountsChanged', handleAccountsChanged)
+          ethereum.removeListener('networkChanged', handleNetworkChanged)
+        }
+      }
+    }
+
+    return () => {}
+  }, [active, error, activate])
   return (
     <div className='App'>
       <Layout>
