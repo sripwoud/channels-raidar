61ea8b5898100ca5e312838e33486f52636f81c2
r1oga <r1oga@tuta.io>
2020-07-04 19:31:09 +0200

    refactor: Extract enable metamask logic in App.js

--- packages/react-app/src/components/Header.js
+++ packages/react-app/src/components/Header.js
@@ -1,58 +1,11 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import { Link, Text, Box, EthAddress, Heading, Image } from 'rimble-ui'
 import { useWeb3React } from '@web3-react/core'
-import { InjectedConnector } from '@web3-react/injected-connector'
 import ConnectionBanner from '@rimble/connection-banner'
-
-const injected = new InjectedConnector({
-  /* Need to linclude all chains available in metamask
-    in supportedChainIds so that the connection banner
-    can display error messages correctly
-  */
-  supportedChainIds: [1, 2, 3, 4, 42, 5]
-})
-
-const { ethereum } = window
 
 export const Header = props => {
   const context = useWeb3React()
-  const { account, chainId, activate, /*deactivate,*/ active, error } = context
-
-  if (ethereum && ethereum.on && !active && !error) {
-    activate(injected)
-  }
-
-  useEffect(() => {
-    if (ethereum && ethereum.on && !active && !error) {
-      const handleChainChanged = chainId => {
-        activate(injected)
-      }
-
-      const handleAccountsChanged = accounts => {
-        if (accounts.length > 0) {
-          activate(injected)
-        }
-      }
-
-      const handleNetworkChanged = networkId => {
-        activate(injected)
-      }
-
-      ethereum.on('chainChanged', handleChainChanged)
-      ethereum.on('accountsChanged', handleAccountsChanged)
-      ethereum.on('networkChanged', handleNetworkChanged)
-
-      return () => {
-        if (ethereum.removeListener) {
-          ethereum.removeListener('chainChanged', handleChainChanged)
-          ethereum.removeListener('accountsChanged', handleAccountsChanged)
-          ethereum.removeListener('networkChanged', handleNetworkChanged)
-        }
-      }
-    }
-
-    return () => {}
-  }, [active, error, activate])
+  const { account, chainId } = context
 
   return (
     <>
