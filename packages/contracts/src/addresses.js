388bcedbe5a44a38082c4c5ac71fefaf76cd8018
r1oga <r1oga@tuta.io>
2020-07-06 10:44:05 +0200

    test: Deploy and interact with contracts locally

--- packages/contracts/src/addresses.js
+++ packages/contracts/src/addresses.js
@@ -2,7 +2,10 @@
 // Goerli, Kovan, Rinkeby and Ropsten. Replace it with your smart contracts.
 const addresses = {
   tttNetwork: '0x3EA2a1fED7FdEf300DA19E97092Ce8FdF8bf66A3',
-  tttErc20: '0x59105441977ecD9d805A4f5b060E34676F50F806'
+  tttErc20: '0x59105441977ecD9d805A4f5b060E34676F50F806',
+  tokenNetworkLocal: '0x630589690929E9cdEFDeF0734717a9eF3Ec7Fcfe',
+  erc20Local: '0xA57B8a5584442B467b4689F1144D269d096A3daF',
+  secretRegistryLocal: '0x5b1869D9A4C187F2EAa108f3062412ecf0526b24'
 }
 
 export default addresses
