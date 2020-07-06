248aa70e820a637103f2b4a856e64542391ac6a5
r1oga <r1oga@tuta.io>
2020-07-06 15:30:26 +0200

    fix: Store copy of contracts package in react-app
    Was preventing deployment to vercel

---
+++ packages/react-app/src/contracts/abis.js
@@ -0,0 +1,9 @@
+import tttNetworkAbi from './abis/tttNetwork.json'
+import tttErc20Abi from './abis/tttErc20.json'
+
+const abis = {
+  tttNetwork: tttNetworkAbi,
+  tttErc20: tttErc20Abi
+}
+
+export default abis
---
+++ packages/react-app/src/contracts/addresses.js
@@ -0,0 +1,11 @@
+// This address points to a dummy ERC20 contract deployed on Ethereum Mainnet,
+// Goerli, Kovan, Rinkeby and Ropsten. Replace it with your smart contracts.
+const addresses = {
+  tttNetwork: '0x3EA2a1fED7FdEf300DA19E97092Ce8FdF8bf66A3',
+  tttErc20: '0x59105441977ecD9d805A4f5b060E34676F50F806',
+  tokenNetworkLocal: '0xe97DbD7116D168190F8A6E7beB1092c103c53a12',
+  erc20Local: '0xA57B8a5584442B467b4689F1144D269d096A3daF',
+  secretRegistryLocal: '0x5b1869D9A4C187F2EAa108f3062412ecf0526b24'
+}
+
+export default addresses
