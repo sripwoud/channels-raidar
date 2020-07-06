320fea14c422d2023b9a294a8c9bc6029120e190
r1oga <r1oga@tuta.io>
2020-07-06 12:06:34 +0200

    feat: Refresh page after displaying event toast

--- packages/react-app/src/components/EventToast.js
+++ packages/react-app/src/components/EventToast.js
@@ -5,36 +5,30 @@ import { useContract } from '../hooks'
 export const EventToast = () => {
   const [event, setEvent] = useState('')
   const [show, setShow] = useState(false)
-  const [toastState, setToastState] = useState('loading')
   const contract = useContract()
 
   if (contract) {
     ;['ChannelOpened', 'ChannelClosed', 'ChannelSettled'].forEach(event => {
       contract.on(event, () => {
-        console.log(`${event} event detected`)
         setEvent(event)
-        setToastState('loading')
         setShow(true)
+        setTimeout(() => {
+          window.location.reload()
+        }, 3000)
       })
     })
   }
 
-  const renderToast = toastState => {
-    switch (toastState) {
-      case 'loading':
-        return (
-          <ToastMessage.Processing
-            message={`Event ${event} detected`}
-            secondaryMessage='Waiting for confirmation'
-          />
-        )
-
-      case 'success':
-        return <ToastMessage.Success message={`Event ${event} confirmed`} />
-
-      default:
-        return <ToastMessage.Failure message='Error' />
-    }
-  }
-  return <>{show ? renderToast(toastState) : <></>}</>
+  return (
+    <>
+      {show ? (
+        <ToastMessage.Processing
+          message={`Event ${event} detected`}
+          secondaryMessage='Page will refresh in 3 seconds...'
+        />
+      ) : (
+        <></>
+      )}
+    </>
+  )
 }
