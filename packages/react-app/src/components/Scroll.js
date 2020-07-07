83fdad1efd220b5f000326aa99a8727dad6af44f
sripwoud <me@sripwoud.xyz>
2020-07-07 23:44:54 +0200

    style: Add media query

--- packages/react-app/src/components/Scroll.js
+++ packages/react-app/src/components/Scroll.js
@@ -1,14 +1,3 @@
 import React from 'react'
 
-export const Scroll = props => {
-  return (
-    <div
-      style={{
-        overflowY: 'scroll',
-        border: '1px white solid',
-        height: '79vh'
-      }}>
-      {props.children}
-    </div>
-  )
-}
+export const Scroll = props => <div className='scroll'>{props.children}</div>
