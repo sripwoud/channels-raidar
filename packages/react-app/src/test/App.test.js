2196cbc7a111cd62219d4f4331adc0c5544b1f71
sripwoud <me@sripwoud.xyz>
2020-07-07 23:36:44 +0200

    test: Renders without crashing. No filter at start

--- packages/react-app/src/test/App.test.js
+++ packages/react-app/src/test/App.test.js
@@ -1,9 +1,32 @@
 import React from 'react'
 import { render } from '@testing-library/react'
+import ApolloClient from 'apollo-boost'
+import { ApolloProvider } from '@apollo/react-hooks'
+import { StoreProvider } from 'easy-peasy'
+import store from '../store'
 import App from '../components/App'
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />)
-  const linkElement = getByText(/learn react/i)
-  expect(linkElement).toBeInTheDocument()
+const client = new ApolloClient({
+  uri: 'https://api.thegraph.com/subgraphs/name/r1oga/raiden-channels'
+})
+
+const WrappedApp = () => (
+  <ApolloProvider client={client}>
+    <StoreProvider store={store}>
+      <App />
+    </StoreProvider>
+  </ApolloProvider>
+)
+
+test('renders without crashing', () => {
+  const { getByText } = render(<WrappedApp />)
+  expect(getByText('CHANNELS RAIDAR')).toBeInTheDocument()
+})
+
+test('Select inputs are initially set to "All"', () => {
+  const { getByLabelText } = render(<WrappedApp />)
+  const inputAddress = getByLabelText('Address')
+  const inputChannels = getByLabelText('Channels')
+  expect(inputAddress === 'All')
+  expect(inputChannels === 'All')
 })
